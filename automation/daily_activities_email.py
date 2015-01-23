95003ebbee26f715fed1407c49fbfa9a03991d85
Si Chen <sichen@opensourcestrategies.com>
2015-01-23 11:00:07 -0800

    comment box

--- automation/daily_activities_email.py
+++ automation/daily_activities_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-##############################################
+##########################################################################
 # Copyright (c) Open Source Strategies Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
